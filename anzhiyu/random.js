6bc9a7a398c5c7ecc0f0afa1cdd2eb0a057152d3
haorwen <haorwen@qq.com>
2025-06-01 16:56:15 +0000

    Publish Post source/_posts/situation_puzzle.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/01/20/fiddler-1/","2022/08/30/hello-world/"];function toRandomPost(){
+var posts=["2023/01/20/fiddler-1/","2022/08/30/hello-world/","2025/06/01/situation_puzzle/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

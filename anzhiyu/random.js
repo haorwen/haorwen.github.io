1ebbd0b278e1e9f16204b19bc714a0a51920024f
haorwen <haorwen@qq.com>
2025-06-06 13:52:45 +0000

    优化折叠内容显示 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/01/20/fiddler-1/","2022/08/30/hello-world/","2025/06/01/situation_puzzle/","2025/06/01/using_vc/"];function toRandomPost(){
+var posts=["2023/01/20/fiddler-1/","2022/08/30/hello-world/","2025/06/01/using_vc/","2025/06/01/situation_puzzle/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };

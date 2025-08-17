ed70c8da3fa84278918e40c797a70f98ba3df120
haorwen <haorwen@qq.com>
2025-08-17 12:51:33 +0000

    Rename source/_drafts/cnb-action-challenge.md.md to source/_drafts/cnb-action-challenge.md by Qexo Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/17/cnb-action-challenge/","2023/01/20/fiddler-1/","2022/08/30/hello-world/","2025/06/01/situation_puzzle/","2025/06/01/using_vc/"];function toRandomPost(){
+var posts=["2023/01/20/fiddler-1/","2022/08/30/hello-world/","2025/06/01/situation_puzzle/","2025/06/01/using_vc/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
